a6ed15e16345a0f6af91d72024136d4e1641a12f
Jinny <ggab.challenge@gmail.com>
2024-11-29 14:19:57 +0900

    refactor: 파일 위치 변경

---
+++ src/Validation/Lotto.js
@@ -0,0 +1,35 @@
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  validate(numbers) {
+    // console.log('numbers: ', numbers)
+    if (numbers[0] === 0) {
+      throw new Error('[ERROR] 빈 값을 입력하셨습니다.');
+    }
+
+    // if (numbers.length !== 6) {
+    //   throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    // }
+
+    if (numbers.some((number) => Number.isNaN(Number(number)))) {
+      throw new Error('[ERROR] 숫자를 입력하지 않으셨습니다.');
+    }
+
+    if (numbers.some((number) => number === 0 || number > 45)) {
+      throw new Error('[ERROR] 1부터 45까지의 숫자가 아닙니다.');
+    }
+
+    const uniqueWinningNumber = new Set(numbers);
+
+    if (uniqueWinningNumber.size !== numbers.length) {
+      throw new Error('[ERROR] 중복된 숫자를 입력하셨습니다.');
+    }
+  }
+}
+
+export default Lotto;
