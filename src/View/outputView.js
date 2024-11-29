6b533206e115e70197cc1826eff781d82ce59fea
Jinny <ggab.challenge@gmail.com>
2024-11-29 14:18:58 +0900

    fix: Console.readlineAsync -> Console.print 변경

--- src/View/outputView.js
+++ src/View/outputView.js
@@ -4,7 +4,7 @@ import { MATCHES } from '../Constant/constants.js';
 export class OutputHandler {
   printPurchaseCount(purchaseCount) {
     // TODO: 리펙토링 - 상수처리
-    return Console.readLineAsync(`\n${purchaseCount}개를 구매했습니다.\n`);
+    return Console.print(`\n${purchaseCount}개를 구매했습니다.\n`);
   }
 
   printIssuedLottoList(purchasedLottoList) {
