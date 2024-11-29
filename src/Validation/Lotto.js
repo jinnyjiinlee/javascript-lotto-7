3fd6bdc565fbbc0218e9533cc004e6bd1e304bb6
Jinny <ggab.challenge@gmail.com>
2024-11-29 14:25:56 +0900

    refactor: 당첨번호 유효성 검사 수정

--- src/Validation/Lotto.js
+++ src/Validation/Lotto.js
@@ -7,14 +7,13 @@ class Lotto {
   }
 
   validate(numbers) {
-    // console.log('numbers: ', numbers)
     if (numbers[0] === 0) {
       throw new Error('[ERROR] 빈 값을 입력하셨습니다.');
     }
 
-    // if (numbers.length !== 6) {
-    //   throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    // }
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
 
     if (numbers.some((number) => Number.isNaN(Number(number)))) {
       throw new Error('[ERROR] 숫자를 입력하지 않으셨습니다.');
