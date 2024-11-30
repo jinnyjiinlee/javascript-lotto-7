0eb02fb22ccb3a7fc8066e3f0528b7b8b251ca00
Jinny <ggab.challenge@gmail.com>
2024-11-30 23:40:15 +0900

    refactor: 코드 최적화

--- src/Validation/bonusNumberValidator.js
+++ src/Validation/bonusNumberValidator.js
@@ -1,15 +1,32 @@
+/* eslint-disable max-lines-per-function */
 export class BonusNumber {
   constructor(bonusNumber, winningNumbers) {
     this.validate(bonusNumber, winningNumbers);
   }
 
+  isBlank(value) {
+    return value === '';
+  }
+
+  isNotNumber(value) {
+    return Number.isNaN(Number(value));
+  }
+
+  isNotScope(value) {
+    return value === 0 || value > 45;
+  }
+
   validate(bonusNumber, winningNumbers) {
-    if (bonusNumber === '') {
+    if (this.isBlank(bonusNumber)) {
       throw new Error('[ERROR] 빈 값을 입력하셨습니다.');
     }
 
-    if (Number.isNaN(Number(bonusNumber))) {
+    if (this.isNotNumber(bonusNumber)) {
       throw new Error('[ERROR] 숫자를 입력하지 않으셨습니다.');
+    }
+
+    if (this.isNotScope(bonusNumber)) {
+      throw new Error('[ERROR] 1부터 45까지의 숫자가 아닙니다.');
     }
 
     if (
@@ -18,10 +35,6 @@ export class BonusNumber {
       )
     ) {
       throw new Error('[ERROR] 당청 번호와 중복된 숫자를 입력하셨습니다.');
-    }
-
-    if (bonusNumber === 0 || bonusNumber > 45) {
-      throw new Error('[ERROR] 1부터 45까지의 숫자가 아닙니다.');
     }
   }
 }
--- src/Validation/winningNumberValidator.js
+++ src/Validation/winningNumberValidator.js
@@ -6,12 +6,25 @@ export class Lotto {
     this.#numbers = numbers;
   }
 
+  isBlank(value) {
+    return value[0] === 0;
+  }
+
+  isNotNumber(value) {
+    return !Number.isNaN(Number(value));
+  }
+
+  isNotScope(value) {
+    return value.some((number) => number < 0 || number > 45);
+  }
+
+  // eslint-disable-next-line max-lines-per-function
   validate(numbers) {
-    if (numbers[0] === 0) {
+    if (this.isBlank(numbers)) {
       throw new Error('[ERROR] 빈 값을 입력하셨습니다.');
     }
 
-    if (numbers.some((number) => Number.isNaN(Number(number)))) {
+    if (this.isNotNumber(numbers)) {
       throw new Error('[ERROR] 숫자를 입력하지 않으셨습니다.');
     }
 
@@ -19,13 +32,13 @@ export class Lotto {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
 
+    if (this.isNotScope(numbers)) {
+      throw new Error('[ERROR] 1부터 45까지의 숫자가 아닙니다.');
+    }
+
     const uniqueNumbers = new Set(numbers);
     if (uniqueNumbers.size !== numbers.length) {
       throw new Error('[ERROR] 중복된 숫자를 입력하셨습니다.');
-    }
-
-    if (numbers.some((number) => number < 0 || number > 45)) {
-      throw new Error('[ERROR] 1부터 45까지의 숫자가 아닙니다.');
     }
   }
 }
